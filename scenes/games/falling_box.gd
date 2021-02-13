5cf3e0eb3332401dadaafc6e621c57b6f334a333
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2021-02-13 21:01:22 +0000

    Added better box distribution

--- scenes/games/falling_box.gd
+++ scenes/games/falling_box.gd
@@ -8,9 +8,11 @@ func _ready():
 	Sets up the falling box
 	"""
 	
+	$Area2D.add_to_group("boxes")
+	
 	position = Vector2(
 		randi() % int(get_viewport_rect().size.x),
-		-50
+		-100
 	)
 
 func _process(dt):
@@ -20,5 +22,14 @@ func _process(dt):
 	
 	position.y += speed * dt
 	
+	# Makes the box dissapear when no longer visible
 	if position.y > get_viewport_rect().size.y+100:
 		queue_free()
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("boxes"):
+		position.x = randi() % int(get_viewport_rect().size.x)
+		print("hit a box")
+	else:
+		print("hit a blob")
